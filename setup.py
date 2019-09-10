ffc5a5a15fc1d0888cfe01cc63997742c96631a1
SimpleNumber <juliabubis@gmail.com>
2019-09-10 17:04:43 +0300

    moved from bb

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+'''
+setup.py file for aa_stats
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'AA_stat',
+    version              = 0.1,
+    description          = '''A utility for validation of peptide identification results in proteomics using amino acid counting.''',
+    long_description     = (''.join(open('README.MD').readlines())),
+    author               = 'Julia Bubis & Lev Levitsky',
+    author_email         = 'julia.bubis@gmail.com',
+    install_requires     = ['pyteomics', 'pandas', 'seaborn', 'scipy', 'numpy', 'lxml', 'jinja2'],
+    classifiers          = ['Intended Audience :: Science/Research',
+                            'Programming Language :: Python :: 2.7',
+                            'Programming Language :: Python :: 3',
+                            'Topic :: Education',
+                            'Topic :: Scientific/Engineering :: Bio-Informatics',
+                            'Topic :: Scientific/Engineering :: Chemistry',
+                            'Topic :: Scientific/Engineering :: Physics',
+                            'Topic :: Software Development :: Libraries'],
+    license              = 'License :: OSI Approved :: Apache Software License',
+    packages             = find_packages(),
+    package_data         = {'AA_stat': ['report.template']},
+    entry_points         = {'console_scripts': ['AA_stat=AA_stat:main']}
+    )
