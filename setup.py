323bfbe5a79e750e788a3cc9638e9b1bb2270069
Lev Levitsky <lev.levitsky@phystech.edu>
2021-02-25 02:55:14 +0300

    Disable adding terminal localizations by default

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                 = 'AA_stat',
-    version              = '1.7',
+    version              = '1.7.1',
     description          = '''A utility for validation of peptide identification results in proteomics using amino acid counting.''',
     long_description     = (''.join(open('README.MD').readlines())),
     author               = 'Julia Bubis & Lev Levitsky',
