39805b78782b0aeb41866f30e7f782425c81b601
Lev Levitsky <lev.levitsky@phystech.edu>
2024-11-19 19:30:06 +0100

    Update changelog and version

--- AA_stat/version.py
+++ AA_stat/version.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from pyteomics.version import _VersionInfo as VersionInfo
 
-__version__ = '2.6'
+__version__ = '2.6.1'
 
 version_info = VersionInfo(__version__)
 version = __version__
